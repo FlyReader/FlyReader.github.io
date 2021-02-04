67e0ccb4ff0e07b030ceff48925eb96fa9bc10ff
落樱 <devneko@outlook.com>
2021-02-04 22:05:37 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
     lang: 'en-US',
     title: 'Fly Reader',
     description: 'Probably the best UWP reader',
-    base: 'docs',
     head: [
       ['link', { rel: 'icon', href: `picture/logo.png` }],
     ],
