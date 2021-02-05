003c7f73425d27f369c74bd8ddc0e265dd8d8efa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-05 09:46:47 +0000

    Deploy FlyReader/Docs to github.com/FlyReader/Docs.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "9bd976b3f73643e478d003affbf42a2c"
+    "revision": "f54e3af9a60e963d31ce122db11b747e"
   },
   {
     "url": "assets/css/0.styles.14d8d04d.css",
@@ -46,12 +46,28 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
+    "url": "assets/js/10.c0ba0ea7.js",
+    "revision": "9290311b19ee673449cae78c17b13920"
+  },
+  {
+    "url": "assets/js/11.504bf9b4.js",
+    "revision": "909ac1099e421421ebf95509c9347161"
+  },
+  {
+    "url": "assets/js/12.027ec924.js",
+    "revision": "3b328bc1b1e2485f2b28cd4305c40975"
+  },
+  {
+    "url": "assets/js/13.759ae9d7.js",
+    "revision": "8af775c89c8a60e834cc8afc8d0a7363"
+  },
+  {
     "url": "assets/js/2.a548c553.js",
     "revision": "3aab1baccc4c633b4508d23308554f56"
   },
   {
-    "url": "assets/js/3.95aadac3.js",
-    "revision": "3158bf0c546145edd3571d2e6857464a"
+    "url": "assets/js/3.6391a0d0.js",
+    "revision": "3003414039bbc3157fe31d9a75d50e5d"
   },
   {
     "url": "assets/js/4.9ef5a331.js",
@@ -74,16 +90,16 @@ self.__precacheManifest = [
     "revision": "9d76dec40dd6a5237b9724f12657cc7d"
   },
   {
-    "url": "assets/js/9.db3b6e99.js",
-    "revision": "6735dc0aed6f0505cfa7de2122acc78a"
+    "url": "assets/js/9.025a0f11.js",
+    "revision": "4f3ae793c9191cc46034087d195a17d6"
   },
   {
-    "url": "assets/js/app.082d7e9a.js",
-    "revision": "5bbd61dee76673cfbf8a9f1987a63ac3"
+    "url": "assets/js/app.8e4f7f74.js",
+    "revision": "ffc59eb9057eab000755b137443bf8de"
   },
   {
     "url": "en-US/index.html",
-    "revision": "a4a785f3eb62b3d63296aba67f468988"
+    "revision": "a694e0e6c83d2c34ce7fd7517e9938ac"
   },
   {
     "url": "icons/android-chrome-192x192.png",
@@ -114,8 +130,24 @@ self.__precacheManifest = [
     "revision": "5aa78329ac02dacf27385ae850f3c84a"
   },
   {
+    "url": "zh-hans/docs/index.html",
+    "revision": "db4ed8c09cbf548bc27116af5ee5bbeb"
+  },
+  {
+    "url": "zh-hans/docs/Launch-FlyReader-with-URL.html",
+    "revision": "03d9da50c1b53c3af859c4203403541d"
+  },
+  {
+    "url": "zh-hans/docs/Payment-Issues.html",
+    "revision": "646feab9289e7e9dc6784f1429728f08"
+  },
+  {
+    "url": "zh-hans/docs/Using-Cortana-Lunch-FlyReader.html",
+    "revision": "0a2f6bda7fede173dc35ddd22be5a944"
+  },
+  {
     "url": "zh-hans/index.html",
-    "revision": "58493c4fbb1cddfe8134a2765f0e13fa"
+    "revision": "2d6a982f9327aa1c5d0af4c77636a8ad"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
