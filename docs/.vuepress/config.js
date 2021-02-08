9461b7ad6bb69e82dced6a399706e916d4a7e3e5
SakuraNeko <devneko@outlook.com>
2021-02-08 10:35:15 +0800

    更换LOGO，并增加APPLE设备支持

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,11 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/assets/img/logo.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#0078D7' }],
+    ['meta', { name: 'theme-color', content: '#33C0FF' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
     ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
@@ -25,6 +29,7 @@ module.exports = {
     smoothScroll: true,
     lastUpdated: true,
     
+    editLinks: true,
     repo: 'FlyReader/FlyReader.github.io',
     docsDir: 'docs',
     docsBranch: 'master',
@@ -66,11 +71,13 @@ module.exports = {
     }
   },
   plugins: [
-    '@vuepress/back-to-top',
-    '@vuepress/pwa', {
+    ['@vuepress/back-to-top',true],
+    ['@vuepress/pwa', {
         serviceWorker: true,
         updatePopup: true
-    }
+    }],
+    ['@vuepress/medium-zoom', true],
   ],
+  
 }
 
